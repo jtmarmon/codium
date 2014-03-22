0cb2b0f91a00a2f21baf107944b0a306e1337802
Jason Marmon <jtmarmon@gmail.com>
2014-03-22 19:10:01 -0400

    joginrref

---
+++ log-in.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<meta charset ="utf-8">
+		<link rel = "stylesheet" href = "bootstrap/css/bootstrap.css">
+		<link rel = "stylesheet" href = "bootstrap/css/log-in.css">
+	</head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="email" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" class="form-control" placeholder="Password" required>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit"> <a href = "#"> Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
