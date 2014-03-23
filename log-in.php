aed4db12d6392cf8074525350cfada7cd139af02
Greg Carlin <gregthegeek@optonline.net>
2014-03-22 20:09:55 -0400

    Log in stuff

--- log-in.php
+++ log-in.php
@@ -1,3 +1,23 @@
+<?php
+
+if(isset($_POST['email'], $_POST['pass'])) {
+  require_once "db-api/all.php";
+
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
+  $user = getUser($email, $pass);
+  if($user == NULL) {
+    $error = "Invalid email or password.";
+  } else {
+    $hash = $user->startSession();
+    $expiration = time() + (60 * 60 * 24 * 3);
+    setcookie('hash', $hash, $expiration, "/");
+    header("Location: dashboard.html");
+    die();
+  }
+}
+
+?>
 <html>
 	<head>
 		<meta charset ="utf-8">
@@ -9,12 +29,13 @@
 
     <div class="container">
 
-      <form class="form-signin" role="form">
+      <form class="form-signin" role="form" action="#" method="post">
         <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
+        <?php if(isset($error)) echo '<div class="error">' . $error . '</div>'; ?>
+        <input type="email" class="form-control" placeholder="Email address" id="email" name="email" required autofocus />
+        <input type="password" class="form-control" placeholder="Password" id="pass" name="pass" required />
         <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me" /> Remember me
         </label>
         <button class="btn btn-lg btn-primary btn-block" type="submit"> <a href = "#"> Sign in</button>
       </form>
