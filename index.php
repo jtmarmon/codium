c4295368eb182aa6b003d17513bb35b05626ea82
Greg Carlin <gregthegeek@optonline.net>
2014-03-23 09:55:09 -0400

    lol

--- index.php
+++ index.php
@@ -29,9 +29,6 @@ if(isset($_COOKIE['hash'])) {
     <link href="bootstrap/css/cover.css" rel="stylesheet">
     <link href="landing.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
